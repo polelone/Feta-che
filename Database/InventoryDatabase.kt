98eb893557ddf5332c17de5794dcddc19aea2357
Pole Lone <unsnate@gmail.com>
2025-05-09 11:31:44 +0900

    Update InventoryDatabase.kt

--- Database/InventoryDatabase.kt
+++ Database/InventoryDatabase.kt
@@ -1,9 +1,15 @@
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-// data Package argument
-// Another entities class, update version 1, Keep schema false
+/* @-data Package argument
+part DB entities class, DB update 1, Keep schema false */
 @Database(entities = [Item:class], version = 1, exportSchema = false)
 
-// InventoryDatabase -> RoomDatabase
+// Base generate DB -> InventoryDatabase
 abstract class InventoryDatabase : RoomDatabase() {}
+  // define a companion object, 
+  abstract fun userDao() : UserDao
+  //
+  companion object {
+    
+  }
